a824d2f5e65e1373f93eb6e8ffb0271fafc69dfa
ckaylor33 <kaylor33@hotmail.co.uk>
2021-02-23 10:54:47 +0000

    Updates

---
+++ precache-manifest.5603e15f758fd130503972556564691a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ccb90c8c6e7d4f4787ac946c7e00ab",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "46611f9dba6bed00ca20",
+    "url": "/hacker-news/static/css/main.9c59207b.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/hacker-news/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46611f9dba6bed00ca20",
+    "url": "/hacker-news/static/js/main.7392e9cc.chunk.js"
+  },
+  {
+    "revision": "1673c2e233d4c15aeda0",
+    "url": "/hacker-news/static/js/runtime-main.2c7535f1.js"
+  }
+]);
